cf1894d4550210429f0f3ce5fa14bc7e8eec5116
keita1988 <keita12may@gmail.com>
2020-10-24 15:23:34 +0900

    商品一覧表示機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # has_many :items
-  # has_many :buyers
+  has_many :items
+  has_many :buyers
 
   with_options presence: true do
     validates :nickname
